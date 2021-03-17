1eb24e9d7f897dc1d0bd87387460e8b422f0e9cc
Matan Mashraki <mail@matanmashraki.com>
2021-03-17 23:07:22 +0200

    Added custom marker supper

--- src/Marker.js
+++ src/Marker.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
-import { Marker } from 'react-google-maps';
+import { Marker, OverlayView } from 'react-google-maps';
 
 class MapViewMarker extends Component {
-  state = {
-    isOpen: false,
-  };
-  showCallout() {
-    this.setState({ isOpen: true });
-  }
-  hideCallout() {
-    this.setState({ isOpen: false });
-  }
   render() {
-    const { description, title, coordinate, onPress, ...rest } = this.props;
-
-    const childrenWithProps = React.Children.map(this.props.children, child => {
-      return React.cloneElement(child, { hideCallout: this.hideCallout.bind(this) });
-    });
+    const { description, title, coordinate, children, ...rest } = this.props;
+    if (children) {
+      const { onPress, ...overlayRest } = rest;
+      const wrappedChildren = onPress ? (
+        <div ref={ref => ref && google.maps.OverlayView.preventMapHitsFrom(ref)} onClick={onPress}>
+          {children}
+        </div>
+      ) : (
+        children
+      );
+      return (
+        <OverlayView
+          {...overlayRest}
+          title={description ? `${title}\n${description}` : title}
+          position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
+          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+          {wrappedChildren}
+        </OverlayView>
+      );
+    }
     return (
       <Marker
         {...rest}
         title={description ? `${title}\n${description}` : title}
         position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
-        onClick={onPress}>
-        {this.state.isOpen && childrenWithProps}
-      </Marker>
+      />
     );
   }
 }
